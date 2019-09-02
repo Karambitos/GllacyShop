49dfde305ce26892b9d5df64e5010b0995208e7f
Karambitos <pase4nikdv@gmail.com>
2019-09-02 19:47:14 +0300

    2

---
+++ script/map.js
@@ -0,0 +1,16 @@
+var map = document.querySelector(".JSmap");
+// var mapInfo = document.querySelector(".contacts-conteiner");
+
+
+					// POPUP catalog
+
+map.addEventListener ("click", function (evt) {
+    evt.preventDefault();
+    console.log(да)
+	// mapInfo.add("modal-show");
+});
+
+// login_popup.addEventListener ("mouseleave", function (evt) {
+// 	evt.preventDefault();
+// 	login_popup.classList.remove("modal-show");
+// });
